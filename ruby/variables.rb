beb0a1fac02297d9173b0c6e333d4a1837ef9532
Thomsen <changsen4584@163.com>
2012-03-26 11:04:25 +0800

    add ruby code

---
+++ ruby/variables.rb
@@ -0,0 +1,14 @@
+puts (5 / 3)
+puts (-5 / 3)
+puts (5 / -3)
+puts (-5 / -3)
+
+puts (5 % 3)
+puts (-5 % 3)
+puts (5 % -3)
+puts (-5 % -3)
+
+puts nil || 2012
+puts "null" || 2012
+puts true || 2012
+puts false || 2012

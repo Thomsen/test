0c25f59ca2a24bb5a98bba21061663b16721fa74
Thomsen <changsen4584@163.com>
2012-03-26 10:58:01 +0800

    java interface params

---
+++ java/src/com/anhuioss/java/inface/IComputingListener.java
@@ -0,0 +1,13 @@
+package com.anhuioss.java.inface;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thom
+ * Date: 12-3-26
+ * Time: 上午10:28
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IComputingListener {
+    
+    public int add(int a, int b);
+}

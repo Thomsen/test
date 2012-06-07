0ca9ffe12f3599120696c3f6e823e3e2f18724cb
Thomsen <changsen4584@163.com>
2012-06-07 15:55:34 +0800

    rm objc m and regex

--- ruby/basic/regex.rb
+++ ruby/basic/regex.rb
@@ -19,3 +19,15 @@ end
 puts "my nick name thom"  =~ /thom/
 
 puts /thom/.match("my nick name thom")
+
+puts /\d/.match("1a2h3")
+
+puts /\d/.match("a2h3")
+
+#puts /\\d/.match("1a2h3")
+
+#puts /\\d/.match("4b3cd")
+
+puts /[\d]*/.match("1a2h3")
+
+#puts /[\\d]/.match("1a2h3")

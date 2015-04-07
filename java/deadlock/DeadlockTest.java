7a584fb6e83b245be3631331fc8edfb41237a616
Thomsen <changsen4584@163.com>
2015-04-07 16:59:03 +0800

    deadlock sync desc， memoryleak content.xml

--- java/deadlock/DeadlockTest.java
+++ java/deadlock/DeadlockTest.java
@@ -11,14 +11,14 @@ public class DeadlockTest {
 
         Thread thread1 = new Thread(new Runnable() {
                 public void run() {
-                    synchronized (lock1) {
+                    synchronized (lock1) {  // use
                         System.out.println("Thread1 acquired lock1");
                         try {
                             TimeUnit.MILLISECONDS.sleep(500);
                         } catch (InterruptedException ignore) {
 
                         }
-                        synchronized (lock2) {
+                        synchronized (lock2) { // require
                             System.out.println("Threadq acquired lock2");
                         }
                     }
@@ -28,9 +28,9 @@ public class DeadlockTest {
 
         Thread thread2 = new Thread(new Runnable() {
                 public void run() {
-                    synchronized (lock2) {
+                    synchronized (lock2) { // use
                         System.out.println("Thread2 acquired lock2");
-                        synchronized (lock1) {
+                        synchronized (lock1) { // requrie 
                             System.out.println("Thread2 acquired lock1");
                         }
                     }

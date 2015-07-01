76f84746470f1aa0f59d3a8f5faf27c519d76109
Thomsen <changsen4584@163.com>
2015-07-01 22:37:08 +0800

    msgid_ds to msqid_ds, ctime segmentation fault

--- c/process/msgtest.c
+++ c/process/msgtest.c
@@ -3,8 +3,9 @@
 #include <sys/types.h>
 #include <sys/msg.h>
 #include <unistd.h>
+#include <errno.h> // find /usr/include -name errno.h
 
-void msg_stat(int, struct msgid_ds);
+void msg_stat(int, struct msqid_ds);
 
 main()
 {
@@ -22,30 +23,43 @@ main()
     char mtext[10];
   } msg_rbuf;
 
-  struct msgid_ds msg_ginfo, msg_sinfo;
+  struct msqid_ds msg_ginfo, msg_sinfo;
 
   char* msgpath = "/tmp/msgqueue";
+  //key = ftok(msgpath, 'a');
 
-  key = IPC_CREAT | IPC_EXCL;
-  msgid = msgget(key, gflags|00666);
+  struct timeval timenow;
+  gettimeofday(&timenow, NULL);
+  key = (key_t) timenow.tv_usec;
+  gflags = IPC_CREAT | IPC_EXCL;
+  msgid = msgget(key, gflags|0666);
   if (msgid == -1) {
-    printf("msg create error\n");
+    //printf("msg create error\n");
+    fprintf(stderr, "msg create failed with error: %d\n", errno); // 17 EEXIST
+    if (errno == EEXIST) {
+      // ll /dev/mqueue
+      reval = msgctl(msgid, IPC_RMID, NULL);
+      if (reval == -1) {
+        printf("unlink msg queue error\n");
+        return ;
+      }
+    }
     return;
   }
 
   msg_stat(msgid, msg_ginfo);
-  sflags = IPC_NOWAIT:
 
+  sflags = IPC_NOWAIT; // <bits/ipc.h>
   msg_sbuf.mtype = 10;
   msg_sbuf.mtext[0] = 'a';
 
   reval = msgsnd(msgid, &msg_sbuf, sizeof(msg_sbuf.mtext), sflags);
-
   if (reval == -1) {
     printf("message send error\n");
   }
 
   msg_stat(msgid, msg_ginfo);
+
   rflags = IPC_NOWAIT|MSG_NOERROR;
   reval = msgrcv(msgid, &msg_rbuf, 4, 10, rflags);
 
@@ -56,6 +70,7 @@ main()
   }
 
   msg_stat(msgid, msg_ginfo);
+
   msg_sinfo.msg_perm.uid =8;
   msg_sinfo.msg_perm.gid = 8;
   msg_sinfo.msg_qbytes = 16388;
@@ -75,7 +90,7 @@ main()
   }
 }
 
-void msg_stat(int msgid, struct msgid_ds msg_info)
+void msg_stat(int msgid, struct msqid_ds msg_info)
 {
   int reval;
   sleep(1);
@@ -92,9 +107,11 @@ void msg_stat(int msgid, struct msgid_ds msg_info)
 
   printf("pid of last msgsnd is %d\n", msg_info.msg_lspid);
   printf("pid of last msgrcv is %d\n", msg_info.msg_lrpid);
-  printf("last msgsnd time is %s\n", ctime(&(msg_info.msg_stime)));
-  printf("last msgrcv time is %s\n", ctime(&(msg_info.msg_rtime)));
+
+  //printf("last msgsnd time is %s\n", ctime(&(msg_info.msg_stime)));
+  //printf("last msgrcv time is %s\n", ctime(&(msg_info.msg_rtime)));
   printf("last change time is %s\n", ctime(&(msg_info.msg_ctime)));
   printf("msg uid is %d\n", msg_info.msg_perm.uid);
   printf("msg gid is %d\n", msg_info.msg_perm.gid);
+
 }

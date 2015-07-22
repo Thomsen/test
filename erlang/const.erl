1dd1b7eec1f788e7583e2a1a01852b5cfebec298
Thomsen <changsen4584@163.com>
2015-07-22 17:17:06 +0800

    erlang module function

---
+++ erlang/const.erl
@@ -0,0 +1,22 @@
+
+-module(const).
+
+-export([convert/2]).
+
+-export([convert_length/1]).
+
+convert(M, inch) ->
+    M / 2.54;
+convert(N, centimeter) ->
+    N * 2.54.
+
+convert_length({centimeter, X}) ->
+    {inch, X/2.54};
+convert_length({inch, Y}) ->
+    {centimeter, Y*2.54}.
+
+
+
+
+
+
--- erlang/helloworld.erl
+++ erlang/helloworld.erl
@@ -1,6 +1,11 @@
 
 -module(helloworld).
 -export([echo/0]).
+-export([fac/1]).
 echo() ->
     io:format("Hello, world!~n").
 
+fac(1) ->
+    1;
+fac(N) ->
+    N*fac(N-1).

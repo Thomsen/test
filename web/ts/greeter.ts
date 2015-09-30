dae49a6ed778ba725fa5c7c5542461b311813ff0
Thomsen <changsen4584@163.com>
2015-09-30 18:29:39 +0800

    typescript superset

---
+++ web/ts/greeter.ts
@@ -0,0 +1,21 @@
+class Student {
+  fullname: string;
+  constructor(public firstname, public middleinitial, public lastname) {
+    this.fullname = firstname + " " + middleinitial + " " + lastname;
+  }
+}
+
+interface Person {
+  firstname: string;
+  lastname: string;
+}
+
+
+
+function greeter(person : Person) {
+  return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("thom", "D.", "lufei");
+
+document.body.innerHTML = greeter(user);

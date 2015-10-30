dc900dbac35ce1331391bd2af4d2d4699f4da758
Thomsen <changsen4584@163.com>
2015-10-30 16:07:43 +0800

    erlang socket tcp connect

---
+++ erlang/socket/tcp_server.erl
@@ -0,0 +1,27 @@
+-module(tcp_server).
+
+-export([server/0]).
+
+server() ->
+    {ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
+    wait_connect(ListenSocket, 0).
+
+wait_connect(ListenSocket, Count) ->
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    spawn(?MODULE, wait_connect, [ListenSocket, Count+1]),
+    get_request(Socket, [], Count).
+
+get_request(Socket, BinaryList, Count) ->
+    case gen_tcp:recv(Socket, 0, 5000) of
+        {ok, Binary} ->
+            get_request(Socket, [Binary|BinaryList], Count);
+        {error, closed} ->
+            handle(lists:reverse(BinaryList), Count)
+    end.
+
+handle(Binary, Count) ->
+    {ok, Fd} = file:open("log_file_"++integer_to_list(Count), write),
+    file:write(Fd, Binary),
+    file:close(Fd).
+
+% tcp_server:server().

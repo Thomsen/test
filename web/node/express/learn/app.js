2706552ef7441079445d2e5ebd5ebabd22b69849
Thomsen <changsen4584@163.com>
2016-11-03 19:46:15 +0800

    nodejs test with express framework

--- web/node/express/learn/app.js
+++ web/node/express/learn/app.js
@@ -126,6 +126,83 @@ superagent.get(cnodeUrl)
 
   });
 
+var fabonacci = function(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new Error('n should be a Number');
+  }
+
+  if (n < 0) {
+    throw new Error('n should >= 0');
+  }
+
+  if (n > 10) {
+
+  }
+
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fabonacci(n - 1) + fabonacci(n - 2);
+};
+
+app.get('/fib', function(req, res) {
+
+  if (!req.query.n) {
+    throw new Error('should input n parameter');
+  }
+
+  var n = Number(req.query.n);
+
+  try {
+    res.send(String(fabonacci(n)));
+  } catch(e) {
+    res.status(500)
+      .send(e.message);
+  }
+
+});
+
+// benchmark
+
+var int1 = function(str) {
+  return +str;
+}
+var int2 = function(str) {
+  return parseInt(str, 10);
+}
+var int3 = function(str) {
+  return Number(str);
+}
+
+var number = '110';
+var benchmark = require('benchmark');
+
+var suite = new benchmark.Suite;
+
+suite
+  .add('+', function() {
+    int1(number);
+  })
+  .add('parseInt', function() {
+    int2(number);
+  })
+  .add('Number', function() {
+    int3(number);
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({'async': true});
+
+module.exports = app;
 
 app.listen(3000, function() {
   console.log('app is listening at port 3000');

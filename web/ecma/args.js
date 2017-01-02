d8676f1294e7163f5218eed12ad0d29c0641afec
Thomsen <changsen4584@163.com>
2017-01-02 09:43:49 +0800

    javascript arguments array

--- web/ecma/args.js
+++ web/ecma/args.js
@@ -13,11 +13,11 @@ a();
 a(1);
 a(1, 2);
 
-a(2, 3, function() {
+a(2, 3, function () {
   console.log("a callback");
 });
 
-var b = function(d, callback) {
+var b = function (d, callback) {
   var len = arguments.length;
   console.log("fun b args length " + len);
   if (callback instanceof Function) {
@@ -27,6 +27,38 @@ var b = function(d, callback) {
 
 b();
 b(3, 2, 1);
-b(2, function() {
+b(2, function () {
   console.log('b callback');
 });
+
+function argsToArray() {
+  var args = Array.prototype.slice.call(arguments);
+  console.log('args: ');
+  console.log(args);
+
+  var argsa = Array.prototype.slice.call(arguments);
+  console.log('argsa: ');
+  argsa = argsa.toString();
+  console.log(argsa);
+  argsa = argsa.replace('[', '(');
+  argsa = argsa.replace(']', ')');
+  console.log(argsa);
+
+  var argsd = Array.prototype.slice.call(arguments);
+  console.log('argsd: ');
+  console.log(argsd);
+  argsd.join(',');
+  console.log(argsd);
+  var argse = '(' + argsd + ')';
+  console.log(argse);
+
+  var argsc = Array.prototype.slice.call(arguments) | [];
+  console.log('argsc: ');
+  console.log(argsc);  // 0 or 1
+
+  console.log('-------------\n\n');
+}
+
+argsToArray();
+argsToArray(1);
+argsToArray('1', '2', '3');

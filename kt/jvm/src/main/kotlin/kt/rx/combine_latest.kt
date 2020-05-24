9d0f064b82550eb71a1618afc3f6f239ac0de5fc
Thomsen <changsen4584@163.com>
2020-05-24 10:43:00 +0800

    gradle kotlin dsl and empty observable

--- kt/jvm/src/main/kotlin/kt/rx/combine_latest.kt
+++ kt/jvm/src/main/kotlin/kt/rx/combine_latest.kt
@@ -1,8 +1,41 @@
 package rx
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.functions.BiFunction
+import io.reactivex.rxjava3.kotlin.Observables
+import io.reactivex.rxjava3.subjects.PublishSubject
+
 fun main(args: Array<String>) {
 
-    //val name = PublishSubject.create<String>()
+    val name = PublishSubject.create<String>()
+
+    val age = PublishSubject.create<Int>()
+
+    // Can not omit Type parameters and BiFunction
+    Observable.combineLatest<String, Int, String>(
+        name, age, BiFunction { n, a -> "$n - age:${a}" })
+        .subscribe {
+            println("onNext - ${it}")
+        }
+
+    // If you introduce RxKotlin then you can use type inference
+    Observables.combineLatest(name, age) { n, a -> "$n - age:${a}" }
+        .subscribe {
+            println("onNext with RxKotlin - ${it}")
+        }
+
+    // Also we can use Observable array for 1st parameter
+    // but second parameter to be array, it's not cool.
+//    Observables.combineLatest(arrayOf(name, age), {
+//        val n = it[0] as String
+//        val a = it[1] as Int
+//        "$n - age:${a}" })
+//        .subscribe({
+//            println("onNext - ${it}")
+//        })
 
 
+    name.onNext("saito")
+    age.onNext(24)
+    name.onNext("yoshida")
 }
---
+++ kt/jvm/src/main/kotlin/kt/rx/empty.kt
@@ -0,0 +1,30 @@
+package kt.rx
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+fun main() {
+    val upstream = PublishSubject.create<Int>()
+    upstream.doOnDispose { println("Uh, the upstream gets disposed") } // This log will never print.
+
+    val tester = upstream
+        .flatMap { v ->
+            when (v) {
+                0 -> {
+                    println("flatMap produces an empty")
+                    Observable.empty<Int>()
+                }
+                else -> {
+                    println("flatMap produces just value")
+                    Observable.just(v)
+                }
+            }
+        }
+        .test()
+
+    upstream.onNext(0) // Will return a complete in flatMap.
+    upstream.onNext(1) // If the stream dies, we'll never emit this value successfully.
+
+    tester.assertValueCount(1) // Actually, the stream is still alive because flatMap ignores the complete!
+
+}

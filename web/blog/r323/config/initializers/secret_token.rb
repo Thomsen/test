395c6648eaad6279aee0eeda738d1270bfcacdcb
Thomsen <changsen4584@163.com>
2022-10-18 15:17:41 +0800

    delete r223 r323 r414 for dependabot alert

--- web/blog/r323/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-R323::Application.config.secret_token = 'ddf192a66c0c51b7bd5cf058e091d543f94f6430f2d74c05144ab70acdd44cd25d75f9509e75a47d9e155857e3c94bd61161825898637a3a183b2d368e7dc8a5'

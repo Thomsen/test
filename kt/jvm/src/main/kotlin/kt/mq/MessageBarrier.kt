1cfa7b2046659ed394512c85d3c4e8e9cfe4ab46
Thomsen <changsen4584@163.com>
2024-03-04 10:44:12 +0800

    kt message queue sync barrier to run async

--- kt/jvm/src/main/kotlin/kt/mq/MessageBarrier.kt
+++ kt/jvm/src/main/kotlin/kt/mq/MessageBarrier.kt
@@ -3,16 +3,39 @@ package kt.mq
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
+import java.util.function.Predicate
 
-class Message(val runnable: Runnable)
+data class Message(val runnable: Runnable, val isAsync: Boolean)
+
+var condition = Predicate { message: Message? ->
+    message?.isAsync ?: false
+}
 
 class MessageQueue {
     private val queue = LinkedBlockingQueue<Message>()
     private @Volatile var barrier: Boolean = false
 
     fun next(): Message? {
-        if (barrier) return null
-        return queue.take()
+        // if (barrier) return null
+        // return queue.take()
+
+        // Peek at the next message without removing it from the queue
+        val message = queue.peek() ?: return null
+        // If there's a barrier and the message is synchronous, skip it
+        return if (barrier && !message.isAsync) {
+            var foundMessage: Message? = null
+            for (msg in queue) {
+                if (condition.test(msg)) {
+                    foundMessage = msg
+                    queue.remove(msg)
+                    break
+                }
+            }
+            foundMessage
+        } else {
+            // If there's no barrier or the message is asynchronous, process it
+            queue.take()
+        }
     }
 
     fun addMessage(message: Message) {
@@ -51,9 +74,10 @@ fun main() {
         looper.loop()
     }.start()
 
-    messageQueue.addMessage(Message(Runnable { printlnWithTime("Message 1") }))
+    messageQueue.addMessage(Message(Runnable { printlnWithTime("Message 1") }, isAsync = false))
     messageQueue.addBarrier()
-    messageQueue.addMessage(Message(Runnable { printlnWithTime("Message 2") }))
+    messageQueue.addMessage(Message(Runnable { printlnWithTime("Message 2") }, isAsync = false))
+    messageQueue.addMessage(Message(Runnable { printlnWithTime("Message 3") }, isAsync = true))
     Thread.sleep(4000) // Simulate some work
     messageQueue.removeBarrier()
 }
